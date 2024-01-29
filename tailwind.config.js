5eb1b1447cd4edbed0bfbc01fbf23bcce409bac1
imtiyaaz-reech <imtiyaaz@reecheble.com>
2024-01-29 17:32:16 +0200

    chore: Extend tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,26 @@ export default {
 		extend: {
 			height: {
 				screen: "100dvh"
+			},
+			colors: {
+				red: {
+					specno: "#D4190C"
+				},
+				green: {
+					specno: "#0AB169"
+				},
+				orange: {
+					specno: "#F4A236"
+				},
+				blue: {
+					specnoLight: "#A7DAFF",
+					specnoNormal: "#489DDA",
+					specnoDark: "#164464",
+					specnoDarker: "#0D4477"
+				},
+				yellow: {
+					specno: "#FFD975"
+				},
 			}
 		}
 	},
