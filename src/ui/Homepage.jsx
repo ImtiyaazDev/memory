6a6254000d66ff21b70a04b2370066cffb9cfb00
imtiyaaz-reech <imtiyaaz@reecheble.com>
2024-01-29 18:28:31 +0200

    style: Style background

--- src/ui/Homepage.jsx
+++ src/ui/Homepage.jsx
@@ -1,3 +1,20 @@
 export default function Homepage() {
-	return <h1>Home</h1>;
+	return (
+		<div className="radial-lg relative h-screen w-screen overflow-hidden">
+			<div className="radial-sm-70 absolute left-1/2 top-3/4 aspect-square w-[650px] rounded-full blur-3xl">
+				&nbsp;
+			</div>
+			<div className="radial-sm-30 absolute left-1/2 top-3/4 aspect-square w-[650px] rounded-full blur-3xl">
+				&nbsp;
+			</div>
+			<div className="radial-sm absolute -top-1/2 left-1/2 z-0 aspect-square w-[800px] rounded-full blur-3xl">
+				&nbsp;
+			</div>
+			<div className="radial-sm absolute bottom-1/2 right-3/4 z-0 aspect-square w-[620px] rounded-full blur-3xl">
+				&nbsp;
+			</div>
+			
+			<main></main>
+		</div>
+	);
 }
