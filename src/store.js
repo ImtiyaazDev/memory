b03202dbb1ff98498e6875c0c4d50283f8f0b520
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-30 13:34:51 +0200

    feat: Add player reducer to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import playerReducer from "./features/player/playerSlice";
+
 export const store = configureStore({
-	reducer: {}
+	reducer: {
+		player: playerReducer
+	}
 });
