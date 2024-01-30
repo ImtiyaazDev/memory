8d2a4b93fbc50e63caa58c3c67cf28a17edf4c9b
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-30 22:53:59 +0200

    refactor: Update deck array

--- src/features/game/Board.jsx
+++ src/features/game/Board.jsx
@@ -2,63 +2,7 @@ import Blur from "../../ui/Blur";
 import Button from "../../ui/Button";
 import PlayerBalloons from "./../player/PlayerBalloons";
 import PlayerRocket from "./../player/PlayerRocket";
-
-const deck = [
-	{ rank: 2, suit: "clubs", src: "/src/assets/cards/front/clubs/2.png" },
-	{ rank: 3, suit: "clubs", src: "/src/assets/cards/front/clubs/3.png" },
-	{ rank: 4, suit: "clubs", src: "/src/assets/cards/front/clubs/4.png" },
-	{ rank: 5, suit: "clubs", src: "/src/assets/cards/front/clubs/5.png" },
-	{ rank: 6, suit: "clubs", src: "/src/assets/cards/front/clubs/6.png" },
-	{ rank: 7, suit: "clubs", src: "/src/assets/cards/front/clubs/7.png" },
-	{ rank: 8, suit: "clubs", src: "/src/assets/cards/front/clubs/8.png" },
-	{ rank: 9, suit: "clubs", src: "/src/assets/cards/front/clubs/9.png" },
-	{ rank: 10, suit: "clubs", src: "/src/assets/cards/front/clubs/10.png" },
-	{ rank: "A", suit: "clubs", src: "/src/assets/cards/front/clubs/ace.png" },
-	{ rank: "J", suit: "clubs", src: "/src/assets/cards/front/clubs/jack.png" },
-	{ rank: "K", suit: "clubs", src: "/src/assets/cards/front/clubs/king.png" },
-	{ rank: "Q", suit: "clubs", src: "/src/assets/cards/front/clubs/queen.png" },
-	{ rank: 2, suit: "diamonds", src: "/src/assets/cards/front/diamonds/2.png" },
-	{ rank: 3, suit: "diamonds", src: "/src/assets/cards/front/diamonds/3.png" },
-	{ rank: 4, suit: "diamonds", src: "/src/assets/cards/front/diamonds/4.png" },
-	{ rank: 5, suit: "diamonds", src: "/src/assets/cards/front/diamonds/5.png" },
-	{ rank: 6, suit: "diamonds", src: "/src/assets/cards/front/diamonds/6.png" },
-	{ rank: 7, suit: "diamonds", src: "/src/assets/cards/front/diamonds/7.png" },
-	{ rank: 8, suit: "diamonds", src: "/src/assets/cards/front/diamonds/8.png" },
-	{ rank: 9, suit: "diamonds", src: "/src/assets/cards/front/diamonds/9.png" },
-	{ rank: 10, suit: "diamonds", src: "/src/assets/cards/front/diamonds/10.png" },
-	{ rank: "A", suit: "diamonds", src: "/src/assets/cards/front/diamonds/ace.png" },
-	{ rank: "J", suit: "diamonds", src: "/src/assets/cards/front/diamonds/jack.png" },
-	{ rank: "K", suit: "diamonds", src: "/src/assets/cards/front/diamonds/king.png" },
-	{ rank: "Q", suit: "diamonds", src: "/src/assets/cards/front/diamonds/queen.png" },
-	{ rank: 2, suit: "hearts", src: "/src/assets/cards/front/hearts/2.png" },
-	{ rank: 3, suit: "hearts", src: "/src/assets/cards/front/hearts/3.png" },
-	{ rank: 4, suit: "hearts", src: "/src/assets/cards/front/hearts/4.png" },
-	{ rank: 5, suit: "hearts", src: "/src/assets/cards/front/hearts/5.png" },
-	{ rank: 6, suit: "hearts", src: "/src/assets/cards/front/hearts/6.png" },
-	{ rank: 7, suit: "hearts", src: "/src/assets/cards/front/hearts/7.png" },
-	{ rank: 8, suit: "hearts", src: "/src/assets/cards/front/hearts/8.png" },
-	{ rank: 9, suit: "hearts", src: "/src/assets/cards/front/hearts/9.png" },
-	{ rank: 10, suit: "hearts", src: "/src/assets/cards/front/hearts/10.png" },
-	{ rank: "A", suit: "hearts", src: "/src/assets/cards/front/hearts/ace.png" },
-	{ rank: "J", suit: "hearts", src: "/src/assets/cards/front/hearts/jack.png" },
-	{ rank: "K", suit: "hearts", src: "/src/assets/cards/front/hearts/king.png" },
-	{ rank: "Q", suit: "hearts", src: "/src/assets/cards/front/hearts/queen.png" },
-	{ rank: 2, suit: "spades", src: "/src/assets/cards/front/spades/2.png" },
-	{ rank: 3, suit: "spades", src: "/src/assets/cards/front/spades/3.png" },
-	{ rank: 4, suit: "spades", src: "/src/assets/cards/front/spades/4.png" },
-	{ rank: 5, suit: "spades", src: "/src/assets/cards/front/spades/5.png" },
-	{ rank: 6, suit: "spades", src: "/src/assets/cards/front/spades/6.png" },
-	{ rank: 7, suit: "spades", src: "/src/assets/cards/front/spades/7.png" },
-	{ rank: 8, suit: "spades", src: "/src/assets/cards/front/spades/8.png" },
-	{ rank: 9, suit: "spades", src: "/src/assets/cards/front/spades/9.png" },
-	{ rank: 10, suit: "spades", src: "/src/assets/cards/front/spades/10.png" },
-	{ rank: "A", suit: "spades", src: "/src/assets/cards/front/spades/ace.png" },
-	{ rank: "J", suit: "spades", src: "/src/assets/cards/front/spades/jack.png" },
-	{ rank: "K", suit: "spades", src: "/src/assets/cards/front/spades/king.png" },
-	{ rank: "Q", suit: "spades", src: "/src/assets/cards/front/spades/queen.png" },
-	{ rank: "Joker", suit: "Joker", src: "/src/assets/cards/front/joker/joker-black.png" },
-	{ rank: "Joker", suit: "Joker", src: "/src/assets/cards/front/joker/joker-red.png" },
-];
+import { deck } from "./Deck";
 
 export default function Board() {
 	return (
@@ -94,7 +38,18 @@ export default function Board() {
 				</div>
 
 				{/* Main game play */}
-				<div className="grid grid-cols-9 grid-rows-6 self-auto rounded-lg bg-gradient-to-tl from-white/10 to-white/60 px-3 py-3"></div>
+				<div className="grid grid-cols-9 grid-rows-6 self-auto rounded-lg bg-gradient-to-tl from-white/10 to-white/60 px-3 py-3">
+					<div>
+						<img
+							src={deck[0].src}
+							alt=""
+						/>
+						<img
+							src="/src/assets/cards/Card_Back.png"
+							alt=""
+						/>
+					</div>
+				</div>
 
 				{/* Player 2 - Right side */}
 				<div className="flex h-1/2 flex-col gap-4 text-center">
