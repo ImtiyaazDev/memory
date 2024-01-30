5a1b88ef43304af056f92ae822aeb3eb24a68651
imtiyaaz-reech <imtiyaaz@reecheble.com>
2024-01-30 10:46:35 +0200

    refactor: Update input component

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,9 +1,15 @@
-export default function Input({placeholder, onClick}) {
+export default function InputPlayer({ placeholder, onClick, onSubmit }) {
 	return (
-		<input
-			type="text"
-			placeholder={placeholder}
-			className="focus:ring-blue-specnoDark w-full rounded-lg px-4 py-2 font-sans text-neutral-600 outline-0 focus:ring-4"
-		/>
+		<form
+			className="flex justify-between"
+			onSubmit={onSubmit}
+		>
+			<input
+				type="text"
+				placeholder={placeholder}
+				className="focus:ring-blue-specnoDark w-full rounded-lg px-4 py-2 font-sans text-neutral-600 outline-0 focus:ring-4"
+				onClick={onClick}
+			/>
+		</form>
 	);
 }
