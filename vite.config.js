bdaddd2d305483f7ca59ccd241a24b331e701d1a
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-30 13:35:34 +0200

    chore: Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslintPlugin()]
+	plugins: [react()]
 });
