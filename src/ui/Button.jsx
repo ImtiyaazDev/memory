3f0a75f85fdda639de5f78ec8fc9f2196ee8959b
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-30 17:36:08 +0200

    style: Add styling for warning button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,8 @@ export default function Button({ children, disabled, onClick, type, style }) {
 
 	const styles = {
 		primary: `${base} bg-green-specno px-10 py-3 text-xl font-semibold hover:bg-green-500 focus:ring-green-500`,
-		danger: `${base} bg-red-specno z-50 px-3 py-2 font-medium hover:bg-red-700 focus:ring-red-500`
+		danger: `${base} bg-red-specno z-50 px-3 py-2 font-medium hover:bg-red-700 focus:ring-red-500`,
+		warning: `${base} bg-orange-specno z-50 px-3 py-2 font-medium hover:bg-orange-500 focus:ring-orange-300`
 	};
 
 	if (style) {
