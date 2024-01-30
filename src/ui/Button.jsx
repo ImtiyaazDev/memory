a0746f98519280afca69be8c2472e2d526094495
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-30 15:58:39 +0200

    refactor: Update button component for style prop

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,4 +1,4 @@
-export default function Button({ children, disabled, onClick, type }) {
+export default function Button({ children, disabled, onClick, type, style }) {
 	const base =
 		"rounded-lg text-white transition-all duration-300 easi-in outline-0 focus:ring-4";
 
@@ -6,6 +6,18 @@ export default function Button({ children, disabled, onClick, type }) {
 		primary: `${base} bg-green-specno px-10 py-3 text-xl font-semibold hover:bg-green-500 focus:ring-green-500`,
 		danger: `${base} bg-red-specno z-50 px-3 py-2 font-medium hover:bg-red-700 focus:ring-red-500`
 	};
+
+	if (style) {
+		return (
+			<button
+				disabled={disabled}
+				className={`${styles[type]} ${style}`}
+				onClick={onClick}
+			>
+				{children}
+			</button>
+		);
+	}
 
 	return (
 		<button
