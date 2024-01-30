8825c8c585e2c13653ad01efa6f1a30ce13c1fbc
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-30 18:44:21 +0200

    style: Style Players card

--- src/features/game/Board.jsx
+++ src/features/game/Board.jsx
@@ -1,5 +1,7 @@
 import Blur from "../../ui/Blur";
 import Button from "../../ui/Button";
+import PlayerBalloons from "./../player/PlayerBalloons";
+import PlayerRocket from "./../player/PlayerRocket";
 
 export default function Board() {
 	return (
@@ -9,16 +11,46 @@ export default function Board() {
 			<Blur type="normalLg" />
 			<Blur type="normalSm" />
 
-			<header className="ml-auto flex items-center px-6 py-3">
-				<Button type="warning">Restare Game</Button>
-				<Button type="danger">Exit Game</Button>
-			</header>
-
-			<main className="z-50">
+			<header className="ml-auto flex items-center gap-5 px-6 py-3">
 				<div className="flex h-full flex-col items-center justify-start gap-6">
 					<h2 className="z-50 self-auto text-4xl font-semibold text-white">
 						Memory
 					</h2>
+				</div>
+				<Button type="warning">Restare Game</Button>
+				<Button type="danger">Exit Game</Button>
+			</header>
+
+			<main className="z-50 grid h-full grid-cols-[240px_1fr_240px] content-center space-x-5 px-6">
+				{/* Player 1 - Left side */}
+				<div className="flex h-1/2 flex-col gap-4 text-center">
+					<div className="flex flex-col items-center gap-6 rounded-lg bg-gradient-to-tl from-white/10 to-white/60 py-6">
+						<PlayerBalloons />
+						<div className="space-y-3">
+							<p className="text-xl font-semibold text-white">Player 1</p>
+							<p className="text-2xl font-semibold text-white">Score: 10</p>
+						</div>
+					</div>
+					<p className="bg-green-specno rounded-lg py-3 text-xl font-bold text-white">
+						It&apos;s Your Turn
+					</p>
+				</div>
+
+				{/* Main game play */}
+				<div className="self-auto bg-green-200"></div>
+
+				{/* Player 2 - Right side */}
+				<div className="flex h-1/2 flex-col gap-4 text-center">
+					<div className="flex flex-col items-center gap-6 rounded-lg bg-gradient-to-tl from-white/10 to-white/60 py-6">
+						<PlayerRocket />
+						<div className="space-y-3">
+							<p className="text-xl font-semibold text-white">Player 2</p>
+							<p className="text-2xl font-semibold text-white">Score: 10</p>
+						</div>
+					</div>
+					<p className="bg-white rounded-lg py-3 text-xl font-bold text-blue-specnoNormal">
+						It&apos;s Your Turn
+					</p>
 				</div>
 			</main>
 		</div>
