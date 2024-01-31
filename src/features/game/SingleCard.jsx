03a15924998cdd5cfad525a4957e26faad56b08e
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-31 13:27:02 +0200

    refactor: Refactor cards into reusable component

---
+++ src/features/game/SingleCard.jsx
@@ -0,0 +1,30 @@
+export default function SingleCard({
+	card,
+	handleCardChoice,
+	flipped,
+	disableCard
+}) {
+	function handleCardClick() {
+		if (!disableCard) handleCardChoice(card);
+	}
+
+	return (
+		<div className="relative">
+			<div>
+				{/* Front card */}
+				<img
+					src={card.src}
+					alt={`${card.rank} of ${card.suit}`}
+					className={`${flipped ? "rotate-y-front delay-200" : "rotate-y-back"} absolute block w-1/2 transition-transform ease-in`}
+				/>
+				{/* Back card */}
+				<img
+					src="/assets/cards/Card_Back.png"
+					alt="Back card"
+					className={`${flipped ? "rotate-y-back delay-0" : ""} block w-1/2 transition-transform delay-200 ease-in`}
+					onClick={handleCardClick}
+				/>
+			</div>
+		</div>
+	);
+}
