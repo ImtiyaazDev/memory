b2f59b28c8ad90e8e43a0d99f42a364602c37c91
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-01-31 13:23:54 +0200

    feat: Create deck of cards

--- src/features/game/Deck.js
+++ src/features/game/Deck.js
@@ -2,325 +2,379 @@ export const deck = [
 	{
 		rank: 2,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/2.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/2.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 3,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/3.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/3.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 4,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/4.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/4.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 5,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/5.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/5.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 6,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/6.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/6.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 7,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/7.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/7.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 8,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/8.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/8.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 9,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/9.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/9.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 10,
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/10.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/10.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "A",
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/ace.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/ace.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "J",
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/jack.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/jack.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "K",
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/king.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/king.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "Q",
 		suit: "clubs",
-		src: "/src/assets/cards/front/clubs/queen.png",
-		color: "black"
+		src: "/assets/cards/front/clubs/queen.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 2,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/2.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/2.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 3,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/3.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/3.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 4,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/4.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/4.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 5,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/5.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/5.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 6,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/6.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/6.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 7,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/7.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/7.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 8,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/8.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/8.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 9,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/9.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/9.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 10,
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/10.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/10.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "A",
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/ace.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/ace.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "J",
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/jack.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/jack.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "K",
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/king.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/king.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "Q",
 		suit: "diamonds",
-		src: "/src/assets/cards/front/diamonds/queen.png",
-		color: "red"
+		src: "/assets/cards/front/diamonds/queen.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 2,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/2.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/2.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 3,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/3.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/3.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 4,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/4.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/4.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 5,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/5.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/5.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 6,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/6.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/6.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 7,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/7.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/7.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 8,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/8.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/8.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 9,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/9.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/9.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 10,
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/10.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/10.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "A",
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/ace.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/ace.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "J",
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/jack.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/jack.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "K",
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/king.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/king.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: "Q",
 		suit: "hearts",
-		src: "/src/assets/cards/front/hearts/queen.png",
-		color: "red"
+		src: "/assets/cards/front/hearts/queen.png",
+		color: "red",
+		isMatched: false
 	},
 	{
 		rank: 2,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/2.png",
-		color: "black"
+		src: "/assets/cards/front/spades/2.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 3,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/3.png",
-		color: "black"
+		src: "/assets/cards/front/spades/3.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 4,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/4.png",
-		color: "black"
+		src: "/assets/cards/front/spades/4.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 5,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/5.png",
-		color: "black"
+		src: "/assets/cards/front/spades/5.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 6,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/6.png",
-		color: "black"
+		src: "/assets/cards/front/spades/6.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 7,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/7.png",
-		color: "black"
+		src: "/assets/cards/front/spades/7.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 8,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/8.png",
-		color: "black"
+		src: "/assets/cards/front/spades/8.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 9,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/9.png",
-		color: "black"
+		src: "/assets/cards/front/spades/9.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: 10,
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/10.png",
-		color: "black"
+		src: "/assets/cards/front/spades/10.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "A",
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/ace.png",
-		color: "black"
+		src: "/assets/cards/front/spades/ace.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "J",
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/jack.png",
-		color: "black"
+		src: "/assets/cards/front/spades/jack.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "K",
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/king.png",
-		color: "black"
+		src: "/assets/cards/front/spades/king.png",
+		color: "black",
+		isMatched: false
 	},
 	{
 		rank: "Q",
 		suit: "spades",
-		src: "/src/assets/cards/front/spades/queen.png",
-		color: "black"
+		src: "/assets/cards/front/spades/queen.png",
+		color: "black",
+		isMatched: false
 	},
 	{
-		rank: "Joker",
-		suit: "Joker",
-		src: "/src/assets/cards/front/joker/joker-black.png",
-		color: "black"
+		rank: "Black",
+		suit: "joker",
+		src: "/assets/cards/front/joker/joker-black.png",
+		color: "black",
+		isMatched: false
 	},
 	{
-		rank: "Joker",
-		suit: "Joker",
-		src: "/src/assets/cards/front/joker/joker-red.png",
-		color: "red"
+		rank: "Red",
+		suit: "joker",
+		src: "/assets/cards/front/joker/joker-red.png",
+		color: "red",
+		isMatched: false
 	}
 ];
