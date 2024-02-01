f86d8ef286e464ce5e173bdc11be7c7f0f9f2fad
ImtiyaazDev <imtiyaazmac@gmail.com>
2024-02-01 07:54:26 +0200

    feat: Add player slice to redux store

---
+++ src/features/player/playerSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const initialState = {
+	players: [
+		{
+			name: "",
+			matchedPairs: 0,
+			currentTurn: true,
+			id: 1
+		},
+		{
+			name: "",
+			matchPairs: 0,
+			currentTurn: false,
+			id: 2
+		}
+	]
+};
+
+const playerSlice = createSlice({
+	name: "player",
+	initialState,
+	reducers: {
+		updateName: (state, action) => {
+			const { playerId, name } = action.payload;
+			state.players.find((player) => player.id === playerId).name = name;
+		}
+	}
+});
+
+export const { updateName } = playerSlice.actions;
+export default playerSlice.reducer;
+
+export const getPlayers = (state) => state.player.players;
